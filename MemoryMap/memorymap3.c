b1d990ebdc14993b9ad99ed8334109033cd56e97
Abhishek Gupta <60277134+lgabhishek18@users.noreply.github.com>
2020-09-11 21:25:53 +0530

    Add files via upload

---
+++ MemoryMap/memorymap3.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int xyz;
+int main(void){ 
+static int abc;
+return 0; 
+}
+
---
+++ MemoryMap/memorymap4.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int xyz;
+int main(void) { 
+		static int abc = 100; 
+		return 0; 
+}
+
